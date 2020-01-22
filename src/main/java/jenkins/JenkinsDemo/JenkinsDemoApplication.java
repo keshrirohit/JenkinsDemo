8dd2ef27e6b620245d1bf9e59642eda1f8df6b97
rohit <rohit.keshri@decimal.co.in>
2020-01-22 11:07:58 +0530

    dockerFile added

--- src/main/java/jenkins/JenkinsDemo/JenkinsDemoApplication.java
+++ src/main/java/jenkins/JenkinsDemo/JenkinsDemoApplication.java
@@ -4,18 +4,26 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
 
 @SpringBootApplication
+@RestController
 public class JenkinsDemoApplication {
 
 	public static Logger logger=LoggerFactory.getLogger(JenkinsDemoApplication.class);
 
 	@PostConstruct
-	public  init(){
+	public  void init(){
 		logger.info("Start initialization......");
 		logger.info("start After making three project ..... ");
+	}
+
+	@GetMapping("/welcome")
+	public  String welcome(){
+		return  "Welcome to Docker example";
 	}
 	public static void main(String[] args)
 	{
